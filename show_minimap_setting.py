6a2275d7646c9310c0858c6fac81c7daf64f0e45
Antoine ROUSSEL <antoine.roussel@edf.fr>
2025-01-09 10:01:12 +0100

    git_save_config.sh

--- show_minimap_setting.py
+++ show_minimap_setting.py
@@ -20,5 +20,10 @@ I find you have to exit and restart Sublime to load the updated settings.
 class ShowMinimapSetting(sublime_plugin.EventListener):
 	def on_activated(self, view):
 		show_minimap = view.settings().get("show_minimap")
-		if isinstance(show_minimap, bool):
-			view.window().set_minimap_visible(show_minimap)
+		# if isinstance(show_minimap, bool):
+		# 	view.window().set_minimap_visible(show_minimap)
+
+		if show_minimap:
+		    view.window().set_minimap_visible(True)
+    elif show_minimap is not None:
+        view.window().set_minimap_visible(False)
